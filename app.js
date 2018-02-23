7585e93d2ac1fe755b0bc725dd77e20543548d5c
admin <admin@Edwins-MacBook-Pro-2.local>
2018-02-23 09:36:17 +0000

    df

--- app.js
+++ app.js
@@ -142,3 +142,10 @@ var utmodel = {
          tamount: '',
 }
 
+var http = require('http');
+//var express = require('express'),
+    //app = module.exports.app = express();
+
+var server = http.createServer(app);
+//var io = require('socket.io').listen(server);  //pass a http.Server instance
+server.listen(process.env.PORT || process.env.port);
